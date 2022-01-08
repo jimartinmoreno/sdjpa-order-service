4bba5f37a4683b84ad2f9f0ddc80eea26ace7094
Nacho Martin <joseignacio.martinmoreno@gmail.com>
2022-01-08 21:38:19 +0100

    Initial Release

--- src/main/java/guru/springframework/orderservice/domain/OrderHeader.java
+++ src/main/java/guru/springframework/orderservice/domain/OrderHeader.java
@@ -1,6 +1,7 @@
 package guru.springframework.orderservice.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -54,6 +55,15 @@ public class OrderHeader extends BaseEntity {
 
     @OneToMany(mappedBy = "orderHeader", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     private Set<OrderLine> orderLines;
+
+    public void addOrderLine(OrderLine orderLine) {
+        if (orderLines == null) {
+            orderLines = new HashSet<>();
+        }
+
+        orderLines.add(orderLine);
+        orderLine.setOrderHeader(this);
+    }
 
     public String getCustomer() {
         return customer;
